c5fd3c013c088e69e936d560976618b45485804c
AUSTIN-YE <yethusteve217@gmail.com>
2022-12-27 00:36:19 +0200

    first commit

--- client/script.js
+++ client/script.js
@@ -64,7 +64,7 @@ const handleSubmit = async ( e) => {
         const messageDiv = document.getElementById(uniqueId)
         loader(messageDiv)
 
-        const response = await fetch("http://localhost:5000",{ 
+        const response = await fetch("https://openai-codex-dtgi.onrender.com/",{ 
                 method: "POST",
                 headers: {
                         "Content-Type": "application/json"
